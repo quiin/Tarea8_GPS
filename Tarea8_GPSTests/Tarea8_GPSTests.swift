07800e2b00bd4c605d7035ab349eadca9d7769c0
Quiin <carlos.reyna@gmal.com>
2015-11-03 11:36:00 -0600

    Initial Commit

---
+++ Tarea8_GPSTests/Tarea8_GPSTests.swift
@@ -0,0 +1,36 @@
+//
+//  Tarea8_GPSTests.swift
+//  Tarea8_GPSTests
+//
+//  Created by Carlos Alejandro Reyna González on 03/11/15.
+//  Copyright © 2015 itesm. All rights reserved.
+//
+
+import XCTest
+@testable import Tarea8_GPS
+
+class Tarea8_GPSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
